aad6e22b80b7707991f31ef522e60f0b7dd080b7
John Blischak <jdblischak@gmail.com>
2014-11-13 15:55:51 -0600

    Convert probes to fastq format for mapping.

---
+++ convert_probes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+'''
+Convert Illumina probes to fastq format.
+
+Example usage:
+ python convert_probes.py data/HumanHT-12_V4_0_R2_15002873_B.txt > data/ht12_probes.fq
+
+Example output:
+
+@NM_017583.3:TRIM44
+CCTGCCTGTCTGCCTGTGACCTGTGTACGTATTACAGGCTTTAGGACCAG
++
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+
+import sys
+
+file = sys.argv[1]
+manifest = open(file, 'r')
+
+for line in manifest:
+    cols = line.strip().split('\t')
+    if cols[0] == 'Homo sapiens':
+        name = '@' + cols[2] + ':' + cols[11]
+        seq = cols[17]
+        qual = '~' * len(seq)
+        sys.stdout.write(name + '\n' + seq + '\n' + '+\n' + qual + '\n')
+
+manifest.close()
