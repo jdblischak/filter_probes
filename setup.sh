4425526f94e3ff0fd40c930e2029ae7ccb8d68db
John Blischak <jdblischak@gmail.com>
2014-11-13 15:04:34 -0600

    Install software and download data.
    Install:
      -bwa
      -bedtools
    
    Download:
      -HapMap CEU genotypes
      -HapMap CEU allele frequencies
      -Human genome hg19
      -Illumina HT12 array manifestInstall bwa and bedtools.

---
+++ setup.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Install:
+#   -bwa
+#   -bedtools
+
+# Download:
+#   -HapMap CEU genotypes
+#   -HapMap CEU allele frequencies
+#   -Human genome hg19
+#   -Illumina HT12 array manifest
+
+# Installations
+mkdir software
+cd software
+
+# Install bwa
+git clone https://github.com/lh3/bwa.git
+cd bwa
+make
+cd ..
+
+# Install bedtools
+git clone https://github.com/arq5x/bedtools2.git
+cd bedtools2
+make
+cd ..
+
+cd ..
+
+# Download data
+mkdir data
+cd data
+
+# Download HapMap CEU genotypes
+wget http://hapmap.ncbi.nlm.nih.gov/downloads/genotypes/2010-08_phaseII+III/forward/genotypes_chr{1..22}_CEU_r28_nr.b36_fwd.txt.gz
+wget http://hapmap.ncbi.nlm.nih.gov/downloads/genotypes/2010-08_phaseII+III/forward/genotypes_chr{X,Y,M}_CEU_r28_nr.b36_fwd.txt.gz
+
+# Download HapMap CEU allele frequencies
+wget http://hapmap.ncbi.nlm.nih.gov/downloads/frequencies/2010-08_phaseII+III/allele_freqs_chr{1..22}_CEU_r28_nr.b36_fwd.txt.gz
+wget http://hapmap.ncbi.nlm.nih.gov/downloads/frequencies/2010-08_phaseII+III/allele_freqs_chr{X,Y,M}_CEU_r28_nr.b36_fwd.txt.gz
+
+# Download human genome hg19
+rsync -avzuP globus.opensciencedatacloud.org::public/illumina/igenomes/Homo_sapiens/UCSC/hg19/Homo_sapiens/UCSC/hg19/Sequence/WholeGenomeFasta/genome.fa .
+mv genome.fa hg19.fa
+
+# Download Illumina array manifest
+wget http://support.illumina.com/content/dam/illumina-support/documents/downloads/productfiles/humanht-12/humanht-12_v4_0_r2_15002873_b.txt.zip
+unzip humanht-12_v4_0_r2_15002873_b.txt.zip
+
+cd ..
